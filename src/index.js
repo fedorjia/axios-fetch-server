7ab370b3764b0472acfccf211040ba293d1ab0a5
fedor <332762488@qq.com>
2018-12-27 12:53:51 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,98 +1,130 @@
 const axios = require('axios')
-// const qs = require('querystring')
 
-let prefix = ''
+class Fetch {
+	constructor() {
+		this.baseURL = ''
+		this.extra = {}
 
-const instance = axios.create({
-	timeout: 6000,
-	withCredentials: false,
-	headers: {
-		'Accept': 'application/json, text/plain, */*',
-		'Content-Type': 'application/json'
-		// 'Access-Control-Allow-Origin': '*'
-	},
+		this.axiosInstance = axios.create({
+			timeout: 6000,
+			withCredentials: false,
+			headers: {
+				'Accept': 'application/json, text/plain, */*',
+				'Content-Type': 'application/json'
+				// 'Access-Control-Allow-Origin': '*'
+			},
 
-	// transformResponse: [function(data) {
-	// 	return JSON.parse(data);
-	// }],
+			// transformResponse: [function(data) {
+			// 	return JSON.parse(data);
+			// }],
 
-	// validateStatus: function (status) {
-	// 	return status >= 200 && status < 300
-	// }
-});
-
-/**
- * interceptor for request
- */
-instance.interceptors.request.use((config) => {
-	return config;
-}, (err) => {
-	return Promise.reject(err);
-});
-
-/**
- * interceptor for response
- */
-instance.interceptors.response.use((res) => {
-	const data = res.data;
-	if (data.status !== 200) {
-		return Promise.reject({status: data.status, body: data.body})
+			// validateStatus: function (status) {
+			// 	return status >= 200 && status < 300
+			// }
+		});
 	}
-	return data.body
-}, (err) => {
-	let errorMessage = err.message
-	let status = -1
-	if (err.response) {
-		status = err.response.status
-		switch (err.response.status) {
-			case 404: {
-				errorMessage = '404 not found'
-				break
+
+	addRequestInterceptor(func) {
+		this.axiosInstance.interceptors.request.use(func)
+	}
+
+	addResponseInterceptor(func) {
+		this.axiosInstance.interceptors.response.use(func)
+	}
+
+	/**
+	 * default interceptor for request
+	 */
+	addDefaultRequestInterceptor() {
+		this.axiosInstance.interceptors.request.use((config) => {
+			const extra = this.getRequestExtra()
+
+			return {
+				...config,
+				...extra
 			}
-			case 500: {
-				errorMessage = '500 internal error'
-				break
+		}, (err) => {
+			return Promise.reject(err);
+		});
+	}
+
+	/**
+	 * default interceptor for response
+	 */
+	addDefaultResponseInterceptor() {
+		this.axiosInstance.interceptors.response.use((res) => {
+			const data = res.data;
+			if (data.status !== 200) {
+				return Promise.reject({status: data.status, body: null, message: data.body})
 			}
+			return data.body
+		}, (err) => {
+			let errorMessage = err.message
+			let status = -1
+			if (err.response) {
+				status = err.response.status
+				switch (err.response.status) {
+					case 404: {
+						errorMessage = '404 not found'
+						break
+					}
+					case 500: {
+						errorMessage = '500 internal error'
+						break
+					}
+				}
+			}
+			return Promise.reject({status: status, body: null, message: errorMessage})
+		});
+	}
+
+	setRequestBaseURL(baseURL) {
+		this.baseURL = baseURL
+	}
+
+	getRequestBaseURL() {
+		return this.baseURL
+	}
+
+	setRequestExtra(extra = {}) {
+		if (extra.baseURL) {
+			this.baseURL = extra.baseURL
 		}
+		delete extra.baseURL
+		this.extra = extra
 	}
-	return Promise.reject({status: status, body: errorMessage})
-});
 
-
-module.exports = {
-
-	setupRequestProxy(proxy) {
-		prefix = proxy
-	},
+	getRequestExtra() {
+		return this.extra
+	}
 
 	async raw(option) {
 		return axios(option)
-	},
+	}
 
 	async request(option) {
-		option.url = prefix + option.url
-		return await instance(option)
-	},
+		return await this.axiosInstance(this.baseURL + option.url)
+	}
 
 	async get(url, params = {}) {
 		return this.request({method: 'get', url, params})
-	},
+	}
 
 	async post(url, params = {}) {
 		return this.request({method: 'post', url, data: params}) // params
-	},
+	}
 
 	async put(url, params = {}) {
 		return this.request({method: 'put', url, data: params})
-	},
+	}
 
 	async patch(url, params = {}) {
 		return this.request({method: 'patch', url, data: params})
-	},
+	}
 
 	async delete(url, params = {}) {
 		return this.request({method: 'delete', url, data: params})
-	},
+	}
 
 	async all(items) {
 		if(!Array.isArray(items)) {
@@ -109,3 +141,5 @@ module.exports = {
 		return await axios.all(task)
 	}
 }
+
+module.exports = new Fetch()
